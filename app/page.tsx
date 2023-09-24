d9a145fede931f9d68adc8e228cd0971fc7a79f0
noeleon21 <noel.gens1@students.mq.edu.au>
2023-09-24 20:57:30 +1000

    started home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import Sponsor from "@/app/sections/Sponsor";
 import CTA from "@/app/sections/CTA";
 import Contact from "./sections/Contact";
 import Nav from "@/components/Nav";
+import Home1 from "@/app/sections/Home";
 
 export default function Home() {
   return (
     <main className=" bg-primary-black ">
       <Nav />
+      <section className="max-container ">
+        <Home1 />
+      </section>
       <section className="max-container padding">
         <Sponsor />
       </section>
