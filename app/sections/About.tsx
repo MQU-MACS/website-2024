65e274a680246eb871bace60b6f772dd437ca726
noeleon21 <noel.gens1@students.mq.edu.au>
2023-09-24 20:58:52 +1000

    started home page

---
+++ app/sections/About.tsx
@@ -0,0 +1,13 @@
+// export default function About() {
+//     return (
+//         <img
+//         src="/MACS Logo.png"
+//         className="
+//         h-40
+//         w-full
+//         object-contain
+//         "
+//         alt="Macs Logo"
+//       />
+//     );
+// }
