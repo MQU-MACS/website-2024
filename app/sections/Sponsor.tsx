d35f751b9a41a2976487f9f263682f7092cf5c73
PeterLQ <nplongquan@gmail.com>
2023-09-24 12:29:04 +1000

    Replace quotes with &apos;

--- app/sections/Sponsor.tsx
+++ app/sections/Sponsor.tsx
@@ -7,8 +7,8 @@ export default function Sponsor() {
         </h2>
 
         <p className="font-light">
-          Thank you to our sponsors! Their support is what makes MACS's work
-          possible.
+          Thank you to our sponsors! Their support is what makes MACS&apos;s
+          work possible.
         </p>
       </div>
       <div className="grid md:grid-cols-2 gap-24 text-center padding-x">
@@ -27,11 +27,11 @@ export default function Sponsor() {
             <p className="text-primary-black">
               Software is changing the world, and Atlassian is at the center of
               it all. Motivated by honest values, an amazing culture, and
-              consistent revenue growth, we're out to unleash the potential of
-              every team. From Amsterdam and Austin to Sydney and San Francisco,
-              we're looking for people who are powered by passion and eager to
-              do the best work of their lives in a highly autonomous yet
-              collaborative, no B.S. environment.
+              consistent revenue growth, we&apos;re out to unleash the potential
+              of every team. From Amsterdam and Austin to Sydney and San
+              Francisco, we&apos;re looking for people who are powered by
+              passion and eager to do the best work of their lives in a highly
+              autonomous yet collaborative, no B.S. environment.
             </p>
           </div>
         </div>
